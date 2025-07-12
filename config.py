fb694392a465eed05fd5f4b26cad375b5c949ba7
anyaa4078 <anyaa4078@gmail.com>
2025-07-12 09:25:15 +0530

    config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "22632606"))
+API_HASH = os.environ.get("API_HASH", "64184f238c75a61c5ea2b8673f94adc7")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7749997031:AAEByP3esZnQ-zoWHPuGqLZjB23D99-vKWg")
+ADMIN = int(os.environ.get("ADMIN", "7059303745"))
 
 
 # Premium 4GB Renaming Client Config
@@ -19,8 +19,8 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://anyaa4078:pfuVoEO96lySDXJ9@renamer.bjs86wb.mongodb.net/?retryWrites=true&w=majority&appName=Renamer")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "renamer")
 
 
 # Other Variables Config
